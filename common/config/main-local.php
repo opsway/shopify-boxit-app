8f63b958fd7614216ba67569468a07bf18e90d1f
Stanislav Derebcinschi <stder@opsway.com>
2015-04-07 09:37:39 -0400

    Added local config file

---
+++ common/config/main-local.php
@@ -0,0 +1,37 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=boxit',
+            'username' => 'root',
+            'password' => 'password',
+            'charset' => 'utf8',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+                [
+                        'class' => 'yii\log\FileTarget',
+                        'levels' => ['info'],
+                        'categories' => ['info'],
+                        'logFile' => '@app/runtime/logs/info.log',
+                        'maxFileSize' => 1024 * 2,
+                        'maxLogFiles' => 20,
+                ],
+            ],
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
